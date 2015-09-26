d8e6247a2348d5fdec570f991b780c4218a74170
rahul-matta <rahulmatta2016@u.northwestern.edu>
2015-09-26 13:33:07 -0500

    added user auth to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925165857) do
+ActiveRecord::Schema.define(version: 20150926181955) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
